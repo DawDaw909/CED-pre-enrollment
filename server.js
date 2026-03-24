938cb4d7d1ea5df85c8ff90f9f8c4972a76686e8
Kar <karizzaabaring@su.edu.ph>
2026-03-24 14:36:59 +0800

    trying to get railway to detect ui

--- server.js
+++ server.js
@@ -23,6 +23,12 @@ app.use('/api/admin', adminRoutes);
 
 const PORT = process.env.PORT || 5000;
 
+const path = require('path');
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
